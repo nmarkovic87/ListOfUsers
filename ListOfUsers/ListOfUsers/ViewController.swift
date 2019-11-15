ad80fbfa4d3d136cbc2e4c4224b17e0c0b631c12
Nemanja Markovic <markovicnemanja1987@gmail.com>
2019-11-15 23:56:08 +0100

    Initial project setup

---
+++ ListOfUsers/ListOfUsers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListOfUsers
+//
+//  Created by Nemanja Markovic on 15/11/2019.
+//  Copyright © 2019 nmarkovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
