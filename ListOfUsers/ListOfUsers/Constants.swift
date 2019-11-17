8944938df199637df336670161c6b41051c370c2
Nemanja Markovic <markovicnemanja1987@gmail.com>
2019-11-18 00:05:35 +0100

    New Constants file

---
+++ ListOfUsers/ListOfUsers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ListOfUsers
+//
+//  Created by Nemanja Markovic on 17/11/2019.
+//  Copyright © 2019 nmarkovic. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let CELL_HEIGHT = 70.0
+    static let CELL_INDETIFIER = "userCell"
+    static let BASE_SIZE = 127397
+    
+}
