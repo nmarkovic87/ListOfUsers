e527edb1aea73c6ea75a2e4ff7bf7f81cf09368a
Nemanja Markovic <markovicnemanja1987@gmail.com>
2019-11-17 12:58:22 +0100

    New extension for UInt64

---
+++ ListOfUsers/ListOfUsers/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  ListOfUsers
+//
+//  Created by Nemanja Markovic on 17/11/2019.
+//  Copyright © 2019 nmarkovic. All rights reserved.
+//
+
+import Foundation
+
+extension UInt64 {
+
+    func megabytes() -> UInt64 {
+        return self * 1024 * 1024
+    }
+}
+
