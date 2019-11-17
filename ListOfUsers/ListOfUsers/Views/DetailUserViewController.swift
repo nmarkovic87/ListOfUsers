c43f4282ab66b70c1862cc45d6da529c995dbceb
Nemanja Markovic <markovicnemanja1987@gmail.com>
2019-11-17 17:51:21 +0100

    Fixed method for sending email

--- ListOfUsers/ListOfUsers/Views/DetailUserViewController.swift
+++ ListOfUsers/ListOfUsers/Views/DetailUserViewController.swift
@@ -9,31 +9,40 @@
 import UIKit
 import MessageUI
 
-class DetailUserViewController: UIViewController, MFMailComposeViewControllerDelegate {
+class DetailUserViewController: UITableViewController, MFMailComposeViewControllerDelegate {
     
     @IBOutlet weak var largePictureImageView: UIImageView!
     @IBOutlet weak var fullNameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var emailButton: UIButton!
     
+    var currentUserInfo: UserInfo?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.fullNameLabel.text = currentUserInfo?.firstName
+        self.ageLabel.text = String(currentUserInfo!.age)
+        self.emailButton.setTitle(currentUserInfo?.email, for: .normal)
+    }
     @IBAction func openMailComposer(_ sender: Any) {
-        sendEmail()
+        sendEmail(currentUserInfo!.email)
     }
 }
 
 extension DetailUserViewController {
     
-    func sendEmail() {
+    func sendEmail(_ email:String) {
         if MFMailComposeViewController.canSendMail() {
             let mail = MFMailComposeViewController()
             mail.mailComposeDelegate = self
-            mail.setToRecipients(["you@yoursite.com"])
-            mail.setMessageBody("<p>You're so awesome!</p>", isHTML: true)
+            mail.setToRecipients([email])
+            mail.setSubject("New subject")
+            mail.setMessageBody("<p>Best Regards, \(String(describing: currentUserInfo!.firstName)) </p>", isHTML: true)
             
             present(mail, animated: true)
         } else {
