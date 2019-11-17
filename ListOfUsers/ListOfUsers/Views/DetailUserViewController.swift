ccda03adb42596e61a646177867c4e16fe3e9c86
Nemanja Markovic <markovicnemanja1987@gmail.com>
2019-11-17 13:00:48 +0100

    Added DetailUserViewController for displaying user info

---
+++ ListOfUsers/ListOfUsers/Views/DetailUserViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailUserViewController.swift
+//  ListOfUsers
+//
+//  Created by Nemanja Markovic on 16/11/2019.
+//  Copyright © 2019 nmarkovic. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class DetailUserViewController: UIViewController, MFMailComposeViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    @IBAction func openMailComposer(_ sender: Any) {
+        sendEmail()
+    }
+        
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["you@yoursite.com"])
+            mail.setMessageBody("<p>You're so awesome!</p>", isHTML: true)
+
+            present(mail, animated: true)
+        } else {
+            // show alert
+        }
+    }
+
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+}
